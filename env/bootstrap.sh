dfacdd20c740ed00cb730cca54ffb67a63364d03
Holger Woltersdorf <hw@hollo.me>
2017-04-01 23:18:52 +0200

    Adds unix domain test sockets

--- env/bootstrap.sh
+++ env/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-ln -sf /vagrant/env/php-fpm/7.0/network-socket.pool.conf /etc/php/7.0/fpm/pool.d/network-pool.conf
+sudo touch /var/run/php-uds.sock && sudo chown www-data:www-data /var/run/php-uds.sock
+
 ln -sf /vagrant/env/php-fpm/7.1/network-socket.pool.conf /etc/php/7.1/fpm/pool.d/network-pool.conf
+ln -sf /vagrant/env/php-fpm/7.1/unix-domain-socket.pool.conf /etc/php/7.1/fpm/pool.d/unix-domain-socket-pool.conf
 
 sudo service php7.0-fpm restart
 sudo service php7.1-fpm restart
