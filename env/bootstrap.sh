f0ef828c1ecd4ffd2ec8b69be30bbd823b5ee65f
Holger Woltersdorf <hw@hollo.me>
2019-01-28 23:48:05 +0100

    Remove vagrant provision script

--- env/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo touch /var/run/php-uds.sock && sudo chown www-data:www-data /var/run/php-uds.sock
-
-ln -sf /vagrant/env/php-fpm/7.1/network-socket.pool.conf /etc/php/7.1/fpm/pool.d/network-pool.conf
-ln -sf /vagrant/env/php-fpm/7.1/unix-domain-socket.pool.conf /etc/php/7.1/fpm/pool.d/unix-domain-socket-pool.conf
-ln -sf /vagrant/env/php-fpm/7.1/restricted-unix-domain-socket.pool.conf /etc/php/7.1/fpm/pool.d/restricted-unix-domain-socket-pool.conf
-
-sudo service php7.1-fpm restart
