da3df27720befe27c86af43b154919f161411d2c
Holger Woltersdorf <hw@hollo.me>
2019-04-28 13:31:19 +0200

    Remove Xdebug whitelist filtering from test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,18 +21,4 @@
  * SOFTWARE.
  */
 
-if (
-	extension_loaded( 'xdebug' )
-	&& version_compare( '2.6.0', phpversion( 'xdebug' ), '<=' )
-)
-{
-	/** @noinspection PhpUndefinedFunctionInspection */
-	/** @noinspection PhpUndefinedConstantInspection */
-	xdebug_set_filter(
-		XDEBUG_FILTER_CODE_COVERAGE,
-		XDEBUG_PATH_WHITELIST,
-		[dirname( __DIR__ ) . '/src']
-	);
-}
-
 require __DIR__ . '/../vendor/autoload.php';
