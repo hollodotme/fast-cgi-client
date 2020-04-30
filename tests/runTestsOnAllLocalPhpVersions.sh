ba4a591fc26fc3163e323e27255661fc74a827e8
Holger Woltersdorf <hw@hollo.me>
2020-04-30 17:14:42 +0200

    Stop all containers before testing

--- tests/runTestsOnAllLocalPhpVersions.sh
+++ tests/runTestsOnAllLocalPhpVersions.sh
@@ -3,7 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )")"
 cd "$(dirname "${DIR}" )" >/dev/null 2>&1
 
-docker-compose up -d --force-recreate php71 php72 php73 php74
 
 PHP_OPTIONS="-d error_reporting=-1 -d auto_prepend_file=build/xdebug-filter.php"
 
@@ -11,12 +10,20 @@ echo -e "\nPHP interpreter options:\n" ${PHP_OPTIONS} "\n"
 
 echo -e "\n\033[43mRun PHPUnit\033[0m\n"
 echo -e "\n\033[33mOn PHP 7.1\033[0m\n"
+docker-compose down
+docker-compose up -d --force-recreate php71
 docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build
 echo -e "\n\033[33mOn PHP 7.2\033[0m\n"
+docker-compose down
+docker-compose up -d --force-recreate php72
 docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build
 echo -e "\n\033[33mOn PHP 7.3\033[0m\n"
+docker-compose down
+docker-compose up -d --force-recreate php73
 docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
 echo -e "\n\033[33mOn PHP 7.4\033[0m\n"
+docker-compose down
+docker-compose up -d --force-recreate php74
 docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
 
 echo -e "\n\033[43mRun phpstan\033[0m\n"
