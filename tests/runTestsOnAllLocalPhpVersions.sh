a500c81867a072c69c99743bcb76ccd11950d0d8
Holger Woltersdorf <hw@hollo.me>
2020-04-30 11:44:31 +0200

    Add PHPUnit 9 for PHP 7.3 & PHP 7.4 tests

--- tests/runTestsOnAllLocalPhpVersions.sh
+++ tests/runTestsOnAllLocalPhpVersions.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )")"
 cd "$(dirname "${DIR}" )" >/dev/null 2>&1
 
-docker-compose up -d php71 php72 php73 php74
+docker-compose up -d --force-recreate php71 php72 php73 php74
 
 PHP_OPTIONS="-d error_reporting=-1 -d auto_prepend_file=build/xdebug-filter.php"
 
@@ -15,9 +15,9 @@ docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build
 echo -e "\n\033[33mOn PHP 7.2\033[0m\n"
 docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build
 echo -e "\n\033[33mOn PHP 7.3\033[0m\n"
-docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build
+docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
 echo -e "\n\033[33mOn PHP 7.4\033[0m\n"
-docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build
+docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
 
 echo -e "\n\033[43mRun phpstan\033[0m\n"
 docker-compose run --rm phpstan
