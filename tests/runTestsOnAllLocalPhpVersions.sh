01f13637dbf1fe1bd6b6c925e171af25037b15c5
Holger Woltersdorf <hw@hollo.me>
2020-08-12 18:20:40 +0200

    Split phpunit configs by version, #61

--- tests/runTestsOnAllLocalPhpVersions.sh
+++ tests/runTestsOnAllLocalPhpVersions.sh
@@ -12,19 +12,19 @@ echo -e "\n\033[43mRun PHPUnit\033[0m\n"
 echo -e "\n\033[33mOn PHP 7.1\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php71
-docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build
+docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build/phpunit7.xml
 echo -e "\n\033[33mOn PHP 7.2\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php72
-docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build
+docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build/phpunit8.xml
 echo -e "\n\033[33mOn PHP 7.3\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php73
-docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
+docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml
 echo -e "\n\033[33mOn PHP 7.4\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php74
-docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build
+docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml
 
 echo -e "\n\033[43mRun phpstan\033[0m\n"
 docker-compose run --rm phpstan
