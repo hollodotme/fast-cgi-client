b57ea931fbadae64f4b5b3150342095f84560b67
Holger Woltersdorf <hw@hollo.me>
2020-08-13 10:42:12 +0200

    Allow passing options to phpunit when calling the script

--- tests/runTestsOnAllLocalPhpVersions.sh
+++ tests/runTestsOnAllLocalPhpVersions.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${DIR}" )" >/dev/null 2>&1
 
 
 PHP_OPTIONS="-d error_reporting=-1 -d auto_prepend_file=build/xdebug-filter.php"
+PHPUNIT_OPTIONS="$@"
 
 echo -e "\nPHP interpreter options:\n" ${PHP_OPTIONS} "\n"
 
@@ -12,19 +13,19 @@ echo -e "\n\033[43mRun PHPUnit\033[0m\n"
 echo -e "\n\033[33mOn PHP 7.1\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php71
-docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build/phpunit7.xml
+docker-compose exec php71 php ${PHP_OPTIONS} vendor/bin/phpunit7.phar -c build/phpunit7.xml ${PHPUNIT_OPTIONS}
 echo -e "\n\033[33mOn PHP 7.2\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php72
-docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build/phpunit8.xml
+docker-compose exec php72 php ${PHP_OPTIONS} vendor/bin/phpunit8.phar -c build/phpunit8.xml ${PHPUNIT_OPTIONS}
 echo -e "\n\033[33mOn PHP 7.3\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php73
-docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml
+docker-compose exec php73 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml ${PHPUNIT_OPTIONS}
 echo -e "\n\033[33mOn PHP 7.4\033[0m\n"
 docker-compose down
 docker-compose up -d --force-recreate php74
-docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml
+docker-compose exec php74 php ${PHP_OPTIONS} vendor/bin/phpunit9.phar -c build/phpunit9.xml ${PHPUNIT_OPTIONS}
 
 echo -e "\n\033[43mRun phpstan\033[0m\n"
 docker-compose run --rm phpstan
