25350720f1c673cb082d86337afd79f3e7ea2cef
Holger Woltersdorf <hw@hollo.me>
2020-11-23 11:30:59 +0100

    Add new Xdebug coverage filter path constant name

--- build/xdebug-filter.php
+++ build/xdebug-filter.php
@@ -5,9 +5,20 @@ if ( !function_exists( 'xdebug_set_filter' ) )
 	return;
 }
 
-/** @noinspection PhpUndefinedConstantInspection */
-xdebug_set_filter(
-	XDEBUG_FILTER_CODE_COVERAGE,
-	XDEBUG_PATH_WHITELIST,
-	[dirname( __DIR__ ) . '/src']
-);
+if ( defined( 'XDEBUG_PATH_WHITELIST' ) )
+{
+	xdebug_set_filter(
+		XDEBUG_FILTER_CODE_COVERAGE,
+		XDEBUG_PATH_WHITELIST,
+		[dirname( __DIR__ ) . '/src']
+	);
+}
+
+if ( defined( 'XDEBUG_PATH_INCLUDE' ) )
+{
+	xdebug_set_filter(
+		XDEBUG_FILTER_CODE_COVERAGE,
+		XDEBUG_PATH_INCLUDE,
+		[dirname( __DIR__ ) . '/src']
+	);
+}
